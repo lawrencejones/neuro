b46751f30ade9777fd6a306b97f3c6e12b177a4d
Lawrence Jones <lawrjone@gmail.com>
2015-11-05 17:11:12 +0000

    Big cleanup

---
+++ iz/Plotters.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+
+
+def plot_show():
+    """
+    Shows the current plot
+    """
+
+    plt.show()
+
+
+def plot_membrane_potentials(population_vs, duration, plot_figure=None):
+    """
+    Plots the neuron membrane potentials by population
+    """
+
+    plt.figure(plot_figure)
+
+    for index, V in enumerate(population_vs):
+        plt.subplot(211 + index)
+        plt.plot(range(duration), V)
+        plt.title('Population ' + str(index + 1) + ' membrane potentials')
+        plt.ylabel('Voltage (mV)')
+        plt.ylim([-90, 40])
+
+    return plt
+
+
+def plot_firings(neuron_network, duration, plot_figure=None):
+    """
+    Plots the firing events of every neuron in each layer of the network
+    """
+
+    plt.figure(plot_figure)
+
+    for index, layer in neuron_network.layers.items():
+        plt.subplot(211 + index)
+        plt.scatter(layer.firings[:, 0], layer.firings[:, 1] + 1, marker='.')
+        plt.xlim(0, duration)
+        plt.ylabel('Neuron number')
+        plt.ylim(0, layer.N + 1)
+        plt.title('Population ' + str(index + 1) + ' firings')
+
+    return plt
--- iz/QuadraticNetworkDemo.py
+++ iz/QuadraticNetworkDemo.py
@@ -1,34 +1,15 @@
-from QuadraticNetwork import QuadraticNetwork
+from NeuronNetwork import NeuronNetwork, add_dirac_pulse
+from QuadraticLayer import QuadraticLayer
+from Plotters import plot_show, plot_membrane_potentials, plot_firings
+
 import numpy as np
-import numpy.random as rn
-import matplotlib.pyplot as plt
 
 
-def create_two_layer_network(N0, N1):
-
-    F = 50 / np.sqrt(N1)
-    D = 5
-    Dmax = 6
-
-    net = QuadraticNetwork([N0, N1], Dmax)
-
-    net.layers[1].S[0] = np.ones([N1, N0])
-    net.layers[1].factor[0] = F
-    net.layers[1].delay[0] = D * np.ones([N1, N0], dtype=int)
-
-    return net
-
-
-def add_dirac(V, firings, dirac_pulse=30):
-    if firings.size > 0:
-        V[firings[:, 0], firings[:, 1]] = dirac_pulse
-
-
-N1, N2 = 4, 4
-duration = 500
+N1, N2 = 20, 20
+duration = 100
 base_current = 20
 
-net = create_two_layer_network(N1, N2)
+net = NeuronNetwork.create_feed_forward(QuadraticLayer, [N1, N2], scaling_factor=50)
 
 V1 = np.zeros([duration, N1])
 V2 = np.zeros([duration, N2])
@@ -43,39 +24,10 @@ for t in xrange(duration):
     V1[t] = net.layers[0].V
     V2[t] = net.layers[1].V
 
-add_dirac(V1, net.layers[0].firings)
-add_dirac(V2, net.layers[1].firings)
+add_dirac_pulse(V1, net.layers[0].firings)
+add_dirac_pulse(V2, net.layers[1].firings)
 
-# Plot membrane potentials
-plt.figure(1)
-plt.subplot(211)
-plt.plot(range(duration), V1)
-plt.title('Population 1 membrane potentials')
-plt.ylabel('Voltage (mV)')
-plt.ylim([-90, 40])
+plot_membrane_potentials([V1, V2], duration, 1)
+plot_firings(net, duration, 2)
 
-plt.subplot(212)
-plt.plot(range(duration), V2)
-plt.title('Population 2 membrane potentials')
-plt.ylabel('Voltage (mV)')
-plt.ylim([-90, 40])
-plt.xlabel('Time (ms)')
-
-# Raster plots of firings
-plt.figure(3)
-plt.subplot(211)
-plt.scatter(net.layers[0].firings[:, 0], net.layers[0].firings[:, 1] + 1, marker='.')
-plt.xlim(0, duration)
-plt.ylabel('Neuron number')
-plt.ylim(0, N1 + 1)
-plt.title('Population 1 firings')
-
-plt.subplot(212)
-plt.scatter(net.layers[1].firings[:, 0], net.layers[1].firings[:, 1] + 1, marker='.')
-plt.xlim(0, duration)
-plt.ylabel('Neuron number')
-plt.ylim(0, N2 + 1)
-plt.xlabel('Time (ms)')
-plt.title('Population 2 firings')
-
-plt.show()
+plot_show()
