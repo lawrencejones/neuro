513d3532cab1ef8c23210b0b7b033ab9c6972349
Lawrence Jones <lawrjone@gmail.com>
2015-11-24 17:13:51 +0000

    Supports random rewiring of modular network

--- iz/ModularNetwork.py
+++ iz/ModularNetwork.py
@@ -1,5 +1,24 @@
+"""
+Examples
+========
+
+ModularNetwork(120, 1200, 6).rewire_network(0.2).plot() => 6 modules, random rewiring
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def select_random_int(low, high, not_allowed=None):
+    """
+    Generates an integer between low and high, that is not equal to not_allowed
+    """
+
+    r = np.random.randint(low, high)
+    while r == not_allowed:
+        r = np.random.randint(low, high)
+
+    return r
 
 
 class ModularNetwork(object):
@@ -42,6 +61,31 @@ class ModularNetwork(object):
             if (i != j) and self.CIJ[i][j] == 0:
                 count = count + 1
                 self.CIJ[i][j] = 1
+
+    def rewire_network(self, p):
+        """
+        Rewires the network edges with probability p to a node in a different module
+        """
+
+        nodes_in_module = self.n / self.C
+
+        for i, j in self.connected_neurons():
+            if np.random.random_sample() < p:
+                target_module = select_random_int(0, self.C, not_allowed=i % nodes_in_module)
+                target_node = target_module * nodes_in_module + np.random.randint(nodes_in_module)
+
+                self.CIJ[i][j] = 0
+                self.CIJ[i][target_node] = 1
+
+        return self
+
+    def connected_neurons(self):
+        """
+        Returns iterable tuples of (i, j), where there is a connection from neuron i to neuron j in
+        the connectivity matrix
+        """
+
+        return zip(*np.where(self.CIJ == 1))
 
     def plot(self):
         """
