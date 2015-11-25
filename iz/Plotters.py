aa4f987b30c14d2de9591b50f16a8458e23f0e84
Lawrence Jones <lawrjone@gmail.com>
2015-11-25 23:33:54 +0000

    Now plots mean firing rate

--- iz/Plotters.py
+++ iz/Plotters.py
@@ -36,18 +36,22 @@ def plot_module_mean_firing_rate(layer, no_of_modules, resolution=None, title=""
     window_buffer = window_size / 2
     max_spike_time = np.max(layer.firings[:, 0])
     duration = 100 * (1 + max_spike_time / 100)
+    firings = layer.firings
 
     sampling_ts = range(window_buffer, duration - window_buffer, n_steps)
     firing_rates = np.zeros((len(sampling_ts), no_of_modules))
     module_size = layer.N / no_of_modules
 
     for i, t in enumerate(sampling_ts):
-        firings_in_window = np.where(np.logical_and(layer.firings[:, 0] > t - window_buffer,
-                                                    layer.firings[:, 0] < t + window_buffer))[0]
+
+        firings_after_start = firings[firings[:, 0] > t - window_buffer]
+        firings_in_window = firings_after_start[firings_after_start[:, 0] < t + window_buffer]
+
         for module_index, module_base in enumerate(range(0, layer.N, module_size)):
-            firings = np.where(np.logical_and(firings_in_window >= module_base,
-                                              firings_in_window < module_base + module_size))[0]
-            firing_rates[i][module_index] = len(firings) / window_size
+            firings_from_module = np.where(np.logical_and(
+                firings_in_window >= module_base,
+                firings_in_window < module_base + module_size))[0]
+            firing_rates[i][module_index] = len(firings_from_module)
 
     plt.figure(plot_figure).add_subplot(1, 1, 1)
     plt.title(title)
@@ -75,19 +79,19 @@ def plot_membrane_potentials(population_vs, duration, plot_figure=None):
     return plt
 
 
-def plot_firings(layers, duration, plot_figure=None):
+def plot_firings(layer, duration, title="", plot_figure=None):
     """
-    Plots the firing events of every neuron in each layer of the network
+    Plots the firing events of every neuron in the given layer
     """
 
     plt.figure(plot_figure)
 
-    for index, layer in enumerate(layers):
-        plt.subplot(len(layers), 1, 1 + index)
-        plt.scatter(layer.firings[:, 0], layer.firings[:, 1] + 1, marker='.')
-        plt.xlim(0, duration)
-        plt.ylabel('Neuron number')
-        plt.ylim(0, layer.N + 1)
-        plt.title('Population ' + str(index + 1) + ' firings')
+    plt.subplot(1, 1, 1)
+    plt.scatter(layer.firings[:, 0], layer.firings[:, 1] + 1, marker='.')
+    plt.xlim(0, duration)
+    plt.title(title)
+    plt.ylabel('Neuron number')
+    plt.xlabel('Time (ms) + 0s')
+    plt.ylim(0, layer.N + 1)
 
     return plt
